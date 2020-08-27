65be9dc2bc2541abea4e99c04cad3090110166cf
Martín P. <mrpalaciopentucci@udc.edu.ar>
2020-08-27 17:32:17 -0300

    aclaración sobre clase

--- src/main/java/ar/edu/udc/tuds/pw2/base_activeweb/DeleteMe.java
+++ src/main/java/ar/edu/udc/tuds/pw2/base_activeweb/DeleteMe.java
@@ -1,9 +1,11 @@
 package ar.edu.udc.tuds.pw2.base_activeweb;
 
 /**
- * 
- * @author user
+ * Esta clase (y el paquete completo) se pueden eliminar. Están para poder
+ * utilizar este proyecto como Maven Archetype.
+ *
+ * @author UniversidadDelChubut
  */
 public class DeleteMe {
-    
+
 }
